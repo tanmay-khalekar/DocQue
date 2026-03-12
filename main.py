54d51dc26ee1f683039937fe6aecfd5b1730c7a1
tanmay-khalekar <tanmaykhalekar@gmail.com>
2026-03-12 22:55:11 +0530

    Set up docker, session.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-
-# Create FastAPI app instance
-app = FastAPI()
-
-# Define a simple GET endpoint
-@app.get("/")
-def read_root():
-    return {"message": "Hello World"}
